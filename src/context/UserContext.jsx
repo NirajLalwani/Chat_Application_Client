23c1d11c566cf5b29ff04b6ef132f45b05a701bb
NirajLalwani <nirajlalwani2810@gmail.com>
2024-07-13 18:18:22 +0530

    added

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -6,7 +6,7 @@ const UserContextProvider = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(true)
     const [userConversation, setUserConversations] = useState([]);
     const [users, setUsers] = useState([])
-    const [userData, setUserData] = useState({});
+    const [userData, setUserData] = useState(false);
 
     //?It consits data of receiver along with sender and receiver conversation id
     const [messagesData, setMessagesData] = useState({
